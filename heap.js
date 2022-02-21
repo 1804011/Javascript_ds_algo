3f749d67b2762912c3f6c3441ec19ea37e947a69
1804011 <sharifsharifulislam061@gmail.com>
2022-02-21 15:39:06 +0600

    var replaced by let

--- heap.js
+++ heap.js
@@ -1,20 +1,20 @@
-var heap = [];
+let heap = [];
 heap.push(-Infinity);
 function swap(u, v) {
-	var t = heap[u];
+	let t = heap[u];
 	heap[u] = heap[v];
 	heap[v] = t;
 }
 function Push(x) {
 	heap.push(x);
-	var sz = heap.length;
-	var k = sz - 1;
+	let sz = heap.length;
+	let k = sz - 1;
 	while (k) {
-		var u;
+		let u;
 		if (k % 2 == 0) u = k / 2;
 		else u = (k - 1) / 2;
 		if (heap[u] > heap[k]) {
-			var t = heap[u];
+			let t = heap[u];
 			heap[u] = heap[k];
 			heap[k] = t;
 		}
@@ -22,16 +22,16 @@ function Push(x) {
 	}
 }
 function Pop() {
-	var sz = heap.length;
+	let sz = heap.length;
 	sz--;
-	var ans = heap[1];
+	let ans = heap[1];
 	heap[1] = heap[sz];
 	heap.pop();
-	var root = 1;
+	let root = 1;
 	while (root < sz) {
-		var u = 2 * root;
-		var v = 2 * root + 1;
-		var largest = root;
+		let u = 2 * root;
+		let v = 2 * root + 1;
+		let largest = root;
 		if (heap[u] != undefined && heap[u] < heap[root]) largest = u;
 		if (heap[v] != undefined && heap[v] < heap[largest]) largest = v;
 		swap(largest, root);
